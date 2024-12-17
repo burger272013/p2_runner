56143b8482e569e863bdb3cbc93bc171aff87c72
burger272013 <zaynullinmark@gmail.com>
2024-12-17 15:02:45 +0300

    add controler
    сделали движение вперед на w а вправо на D

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+
     void Update()
     {
-        transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime); 
+        if (Input.GetKey(KeyCode.W))
+        { 
+            transform.Translate(new Vector3(0, 0, 10) * Time.deltaTime); 
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(10, 0, 0) * Time.deltaTime);
+        }
     }
+
 }
